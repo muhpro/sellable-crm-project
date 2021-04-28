8d6e190e871c52b0ff53a6ebb6e08b89163e8909
Muhammad Aderoju <aderojumuhammad6@gmail.com>
2021-04-28 17:51:22 +0100

    configure static files

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from leads.views import landing_page
@@ -22,3 +24,6 @@ urlpatterns = [
     path('', landing_page, name="landing-page"),
     path('leads/', include("leads.urls", namespace="leads"))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
