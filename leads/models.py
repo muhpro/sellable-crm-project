b586a2f323884e70f6c735279a248217bd1efbc2
Muhammad Aderoju <aderojumuhammad6@gmail.com>
2021-04-29 16:00:15 +0100

    signals

--- leads/models.py
+++ leads/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
 
 
 
@@ -32,4 +33,9 @@ class Lead(models.Model):
     
     
 
-    
+def post_user_created_signal(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+post_save.connect(post_user_created_signal, sender=User) 
