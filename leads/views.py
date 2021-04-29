ea2d76f6185415022dae0db50bf68c008fef05c8
Muhammad Aderoju <aderojumuhammad6@gmail.com>
2021-04-29 14:31:58 +0100

    Authentication

--- leads/views.py
+++ leads/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, redirect, reverse
 from .models import Lead, Agent
-from .forms import LeadForm, LeadModelForm
 from django.http import HttpResponse
 from django.views import generic
 from django.core.mail import send_mail
+from .forms import LeadForm, LeadModelForm, CustomUserCreationForm
+
+
+class SignupView(generic.CreateView):
+    template_name = "registration/signup.html"
+    form_class = CustomUserCreationForm
+
+    def get_success_url(self):
+        return reverse("login")
+
 
 class LandingPageView(generic.TemplateView):
     template_name = "landing.html"
