d513b05561bc0dd81ee2e53123668da096db4ec4
Muhammad Aderoju <aderojumuhammad6@gmail.com>
2021-04-30 09:24:12 +0100

    add User get_user_model error

--- agents/forms.py
+++ agents/forms.py
@@ -3,6 +3,7 @@ from leads.models import Agent
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
+User = get_user_model()
 
 class AgentModelForm(forms.ModelForm):
     class Meta:
